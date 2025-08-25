fd98d4cf177a09ca88e3f031bd5a612bbfd87e36
Sujanshrestha <sujanstha5732@gmail.com>
2025-08-25 16:14:33 +0545

    commit

--- events/models.py
+++ events/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Events(models.Model):
+    title = models.CharField(max_length=255, blank=False, null=False),
+    description = models.TextField(blank=False, null=False),
+    image = models.ImageField(upload_to='event_images'),
